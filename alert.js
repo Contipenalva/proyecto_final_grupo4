155e8355560efa7f1791a91596848ae72e5323b2
unknown <penalvaconstanza62@gmail.com>
2023-11-06 21:33:24 -0300

    creo el alert.js

---
+++ alert.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function(){
+    document.getElementById("botonAlerta").addEventListener("click", function() {
+      alert("Para comprar tiene que estar registrado");
+      window.open("http://127.0.0.1:5501/registro.html");
+    })
+   })
+   window.addEventListener("load", function(){
+    document.getElementById("botonAlerta2").addEventListener("click", function() {
+      alert("Para agregar a favoritos tiene que estar registrado");
+      window.open("http://127.0.0.1:5501/registro.html");
+    })
+   })
